bee7e382ac4f7437859e7a8837bd560a3eef8112
LavaheartGaming <lavaheart@GLITCHY.localdomain>
2022-01-23 19:45:32 +0100

    help_what_the_hell_is_this

---
+++ snake.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+const int width =20;
+const int height =20;
+int x, y, fruitx, fruity, score, hiscore; 
+enum direction{ STOP = 0, LEFT, RIGHT, UP, DOWN};
+direction dir;
+void init()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitx = rand() % width;
+    fruity = rand() % height;
+    score = 0;
+}
+void input()
+{
+    
+}
+void logic()
+{
+
+}
+void draw()
+{
+
+}
+main(void)
+{
+ init();
+ while(!gameOver)
+ {
+   draw();
+   input();
+   logic();  
+ }
+    return 0;
+}
