aa39e3a70679e8b2f4d0358d6b7bf4c462ed51a1
LavaheartGaming <lavaheart@GCFRLY097.GAMINGCAMPUS.local>
2022-01-28 12:54:32 +0100

    light

---
+++ exercice1.hpp
@@ -0,0 +1,15 @@
+#ifndef EXERCICE1_HPP
+#define EXERCICE1_HPP
+
+#include <iostream>
+#include "SDL2/SDL.h" // Mes autres includes du projet si besoin
+
+class MainSDLWindow  {
+  window();
+  renderer();
+  ~MainSDLWindow();
+  void MyMeth();
+  int *MyMeth2(int a);
+};
+
+#endif
